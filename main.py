8db0f5de2f2ee18575e24667e0916dade186ac20
sryis <eric92111449@gmail.com>
2023-07-27 22:09:05 +0800

    second try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -9,15 +9,12 @@ templates = Jinja2Templates(directory="templates")
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/items/", response_class=HTMLResponse)
+@app.get("/items/", response_class=HTMLResponse)
 async def create_item(request: Request, name: str = None):
     if name is None:
         raise HTTPException(status_code=400, detail="Name field is required")
-    return RedirectResponse(url=f"/result/{name}", status_code=302)
-
-@app.get("/result/{name}", response_class=HTMLResponse)
-def show_result(name: str, request: Request):
     return templates.TemplateResponse("result.html", {"request": request, "name": name})
+
 
 
 
